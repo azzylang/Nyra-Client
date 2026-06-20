e87832763e1c88aedd689fe4f059554722983246
constt <175602385+constttdev@users.noreply.github.com>
2026-06-21 00:09:31 +0200

    feat: set main file to client entrypoint

--- src/main/java/de/constt/nyra/client/NyraMod.java
+++ src/main/java/de/constt/nyra/client/NyraMod.java
@@ -1,20 +1,33 @@
 package de.constt.nyra.client;
 
+import com.jagrosh.discordipc.IPCClient;
+import de.constt.nyra.client.discordRpc.DiscordIPCCore;
+import de.constt.nyra.client.managers.EventManager;
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class NyraMod implements ModInitializer {
+public class NyraMod implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("nyra");
     public static final String VERSION = /*$ mod_version*/ "0.0.1";
-    public static final String MINECRAFT = /*$ minecraft*/ "26.2";
+    public static final String MINECRAFT = /*$ minecraft*/ "26.1";
 
     @Override
-    public void onInitialize() {
-        // initializing
+    public void onInitializeClient() {
+        LOGGER.info("Nyra {} for MC {} initializing...", VERSION, MINECRAFT);
 
+        EventManager.registerEvents();
 
+        // Check if DiscordIPC class loaded
+        try {
+            LOGGER.info("DiscordIPC class: {}", IPCClient.class.getName());
+        } catch (Throwable t) {
+            LOGGER.error("DiscordIPC not found!", t);
+        }
+
+        DiscordIPCCore.start();
     }
 
     /**
