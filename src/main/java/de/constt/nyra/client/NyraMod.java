d8c9cdaf7e80c53c9de68a9f48d72ab631e8bf06
constt <175602385+constttdev@users.noreply.github.com>
2026-06-21 15:25:26 +0200

    feat: chat utils

--- src/main/java/de/constt/nyra/client/NyraMod.java
+++ src/main/java/de/constt/nyra/client/NyraMod.java
@@ -2,9 +2,14 @@ package de.constt.nyra.client;
 
 import com.jagrosh.discordipc.IPCClient;
 import de.constt.nyra.client.discordRpc.DiscordIPCCore;
+import de.constt.nyra.client.impl.ImGuiImpl;
+import de.constt.nyra.client.impl.RenderInterface;
 import de.constt.nyra.client.managers.EventManager;
+import de.constt.nyra.client.utils.MessageUtils;
+import imgui.ImGui;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,12 +17,16 @@ import org.slf4j.LoggerFactory;
 public class NyraMod implements ClientModInitializer {
     public static final Logger LOGGER = LoggerFactory.getLogger("nyra");
     public static final String VERSION = /*$ mod_version*/ "0.0.1";
-    public static final String MINECRAFT = /*$ minecraft*/ "26.1";
+    public static final String MINECRAFT = /*$ minecraft*/ "26.2";
 
     @Override
     public void onInitializeClient() {
         LOGGER.info("Nyra {} for MC {} initializing...", VERSION, MINECRAFT);
 
+        // -- Variable Setting --
+        MessageUtils.setPrefix("Nyra");
+
+        // -- Events --
         EventManager.registerEvents();
 
         // Check if DiscordIPC class loaded
---
+++ src/main/java/de/constt/nyra/client/utils/MessageUtils.java
@@ -0,0 +1,60 @@
+package de.constt.nyra.client.utils;
+
+import net.minecraft.ChatFormatting;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+
+public class MessageUtils {
+    public static String prefix;
+
+    public static String getPrefix(boolean isClean) {
+        if (prefix == null) return "";
+        String result = prefix + "§r | ";
+        if (isClean) {
+            result = result.replaceAll("§.", "");
+            result = result.replaceAll(" \\| $", "");
+        }
+        return result;
+    }
+
+    public static void setPrefix(String newPrefix) {
+        prefix = newPrefix;
+    }
+
+    public static void sendCSMessageNeutral(String msg) {
+        Minecraft mc = Minecraft.getInstance();
+
+        //~ if <26.1 'mc.gui.hud.getChat().addClientSystemMessage(' -> 'mc.gui.getChat().addMessage('
+        mc.gui.hud.getChat().addClientSystemMessage(
+                Component.literal(getPrefix(false))
+                        .append(Component.literal(msg).withStyle(ChatFormatting.GRAY)));
+    }
+
+    public static void sendCSMessageSucess(String msg) {
+        Minecraft mc = Minecraft.getInstance();
+
+        //~ if <26.1 'mc.gui.hud.getChat().addClientSystemMessage(' -> 'mc.gui.getChat().addMessage('
+        mc.gui.hud.getChat().addClientSystemMessage(
+                Component.literal(getPrefix(false))
+                        .append(Component.literal(msg).withStyle(ChatFormatting.GREEN))
+        );
+    }
+
+    public static void sendCSMessageWarning(String msg) {
+        Minecraft mc = Minecraft.getInstance();
+
+        //~ if <26.1 'mc.gui.hud.getChat().addClientSystemMessage(' -> 'mc.gui.getChat().addMessage('
+        mc.gui.hud.getChat().addClientSystemMessage(
+        Component.literal(getPrefix(false))
+                .append(Component.literal(msg).withStyle(ChatFormatting.YELLOW)));
+    }
+
+    public static void sendCSMessageError(String msg) {
+        Minecraft mc = Minecraft.getInstance();
+
+        //~ if <26.1 'mc.gui.hud.getChat().addClientSystemMessage(' -> 'mc.gui.getChat().addMessage('
+        mc.gui.hud.getChat().addClientSystemMessage(
+                Component.literal(getPrefix(false))
+                        .append(Component.literal(msg).withStyle(ChatFormatting.RED)));
+    }
+}
