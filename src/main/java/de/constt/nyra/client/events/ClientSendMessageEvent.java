d787b4d993080f131e09b8f35f986c4f36a4efe0
NotAzex <147281525+azzylang@users.noreply.github.com>
2026-07-01 21:40:53 +0300

    rm: remove unnecessary duplicate EventManager class fix: $clickgui command rm: remove ClientReceiveMessageEvent change: commands are now used in ClientSendMessageEvent listener

--- src/main/java/de/constt/nyra/client/events/ClientSendMessageEvent.java
+++ src/main/java/de/constt/nyra/client/events/ClientSendMessageEvent.java
@@ -1,13 +1,50 @@
 package de.constt.nyra.client.events;
 
+import de.constt.nyra.client.clientcommands.CCommandManager;
+import de.constt.nyra.client.utils.CommandAnnotationUtils;
 import de.constt.nyra.client.utils.MessageUtils;
 import net.fabricmc.fabric.api.client.message.v1.ClientSendMessageEvents;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.player.LocalPlayer;
+
+import java.util.Arrays;
 
 public class ClientSendMessageEvent {
 
     public static void register() {
 
         ClientSendMessageEvents.ALLOW_CHAT.register((message) -> {
+
+            if (message.charAt(0) == '$') {
+                LocalPlayer player = Minecraft.getInstance().player;
+                System.out.println("a");
+                if (player == null) return false;
+                System.out.println("b");
+
+                if (!message.startsWith(CCommandManager.cmdPrefix)) return true;
+
+                String raw = message.substring(CCommandManager.cmdPrefix.length()).trim();
+                String[] split = raw.split(" ");
+
+                String cmdName = split[0];
+                String[] args = split.length > 1
+                        ? Arrays.copyOfRange(split, 1, split.length)
+                        : new String[0];
+
+                for (var commandClass : CCommandManager.getCommands()) {
+                    String name = CommandAnnotationUtils.getCommand(commandClass.getClass());
+
+                    System.out.println("name: " + name + " | cmdName: " + cmdName + " | args: " + Arrays.toString(args));
+
+                    if (name.equalsIgnoreCase(cmdName)) {
+                        commandClass.executeCommand(args);
+                        break;
+                    }
+                }
+
+                return false;
+            }
+
             if (message.charAt(0) == '#') {
 
                 String msg;
@@ -23,9 +60,11 @@ public class ClientSendMessageEvent {
 
                 // finish code when server is obtained
 
+                return false;
+
             }
 
-            return false;
+            return true;
 
         });
     }
--- src/main/java/de/constt/nyra/client/events/EventManager.java
+++
@@ -1,9 +0,0 @@
-package de.constt.nyra.client.events;
-
-public class EventManager {
-    public static void registerEvents() {
-        ClientTickEventsEvent.register();
-        ClientReceiveMessageEvent.register();
-        ClientSendMessageEvent.register();
-    }
-}
