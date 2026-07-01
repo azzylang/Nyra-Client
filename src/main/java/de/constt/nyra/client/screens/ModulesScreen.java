9c9bc192c87fc90a9a7e8f56c43e3bde88de8cbd
constt <175602385+constttdev@users.noreply.github.com>
2026-07-01 20:43:37 +0200

    feat: module settings

--- src/main/java/de/constt/nyra/client/screens/ModulesScreen.java
+++ src/main/java/de/constt/nyra/client/screens/ModulesScreen.java
@@ -2,11 +2,15 @@ package de.constt.nyra.client.screens;
 
 import de.constt.nyra.client.roots.implementations.CategoryImplementation;
 import de.constt.nyra.client.roots.implementations.ModuleImplementation;
+import de.constt.nyra.client.roots.implementations.SettingImplementation;
 import de.constt.nyra.client.roots.modules.ModuleManager;
 import imgui.ImGui;
 import imgui.flag.ImGuiCond;
 
 public class ModulesScreen extends BaseScreen {
+
+    private ModuleImplementation settingsModule;
+
     @Override
     public void render() {
         super.render();
@@ -20,10 +24,16 @@ public class ModulesScreen extends BaseScreen {
 
             if (ImGui.begin(category.name())) {
                 for (ModuleImplementation module : ModuleManager.getModules()) {
-                    if(ModuleManager.getCategory(module.getClass()) == category) {
-                        if(ImGui.button(module.getTranslatableText())) {
-                            module.toggle();
-                        }
+                    if (ModuleManager.getCategory(module.getClass()) != category) {
+                        continue;
+                    }
+
+                    if (ImGui.button(module.getTranslatableText())) {
+                        module.toggle();
+                    }
+
+                    if (ImGui.isItemClicked(1)) {
+                        settingsModule = module;
                     }
                 }
             }
@@ -32,5 +42,37 @@ public class ModulesScreen extends BaseScreen {
 
             offset += 240;
         }
+
+        if (settingsModule != null) {
+            ImGui.setNextWindowSize(300, 400, ImGuiCond.FirstUseEver);
+
+            boolean[] open = {true};
+
+            if (ImGui.begin(settingsModule.getTranslatableText() + " Settings")) {
+
+                renderSettings(settingsModule);
+
+            }
+
+            ImGui.end();
+
+            if (!open[0]) {
+                settingsModule = null;
+            }
+        }
+    }
+
+    private void renderSettings(ModuleImplementation module) {
+        ImGui.beginChild("Settings");
+
+        for (SettingImplementation<?> setting : module.getSettings()) {
+            setting.renderImGui();
+        }
+
+        ImGui.separator();
+
+        module.renderCustomSettings();
+
+        ImGui.endChild();
     }
 }
